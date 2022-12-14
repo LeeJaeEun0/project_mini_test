7343521bd1d348ddb884ba37f8195ba282b394ca
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-14 11:07:34 +0900

    update_sub

--- pro_mini_test/data_app.py
+++ pro_mini_test/data_app.py
@@ -15,3 +15,14 @@ if st.button('data copyright link'):
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)  
+
+st.subheader('역별 하차 인원')
+option = st.selectbox(
+    'Select Line', 
+    (df['역명']))
+
+station_data = df.loc[(df['역명'] == option)]
+station_data = station_data[station_data.columns.difference(['연번', '호선', '역번호', '역명'])]
+s_index = station_data.index.tolist()
+st.area_chart(station_data.loc[s_index[0]], use_container_width=True
+)
