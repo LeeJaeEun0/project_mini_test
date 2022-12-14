dbe9d6dded0711e862222e741219557f00df01c6
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-14 11:27:24 +0900

    update_down

--- pro_mini_test/data_app.py
+++ pro_mini_test/data_app.py
@@ -16,13 +16,14 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)  
 
-st.subheader('역별 하차 인원')
+st.subheader('호선별 하차 인원')
+df_line = df.groupby('호선').sum()
+lines = df_line.index.tolist()
 option = st.selectbox(
     'Select Line', 
-    (df['역명']))
+    (lines))
 
-station_data = df.loc[(df['역명'] == option)]
-station_data = station_data[station_data.columns.difference(['연번', '호선', '역번호', '역명'])]
-s_index = station_data.index.tolist()
-st.area_chart(station_data.loc[s_index[0]], use_container_width=True
-)
+line_data = df_line.loc[(df_line.index == option)]
+line_data = line_data[line_data.columns.difference(['연번', '호선', '역번호', '역명'])]
+l_index = line_data.index.tolist()
+st.area_chart(line_data.loc[l_index[0]], use_container_width=True)
