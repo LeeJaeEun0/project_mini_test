aad1d56f33d5d74fed32b7637b1c9d6a8ab81a4e
Lee JaeEun <115062865+LeeJaeEun0@users.noreply.github.com>
2022-12-14 09:49:13 +0900

    update_csv

--- pro_mini_test/data_app.py
+++ pro_mini_test/data_app.py
@@ -3,3 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+st.title('2021 서울교통공사 지하철 월별 하차 인원')
+
+df = pd.read_csv('./monthly_subway_statistics_in_seoul.csv', encoding='CP949')
+df.set_index = df['연번']
+
+# if st.button('data copyright link'):
+#     st.write('https://www.data.go.kr/data/15044247/fileData.do')
